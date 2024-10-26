b78cd722bd3895b3e64bb676792af9f16ec78ffe
Garik <garikmartikyan03@gmail.com>
2024-10-26 13:55:31 +0400

    added Splashscreen

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {TabNavigator} from './navigation/TabNavigator.tsx';
 
-// import BootSplash from 'react-native-bootsplash';
+import BootSplash from 'react-native-bootsplash';
 
 export function App() {
-  // useEffect(() => {
-  //   const init = async () => {
-  //     // …do multiple sync or async tasks
-  //   };
-  //
-  //   init().finally(async () => {
-  //     await BootSplash.hide({fade: true});
-  //     console.log('BootSplash has been hidden successfully');
-  //   });
-  // }, []);
+  useEffect(() => {
+    const init = async () => {
+      // …do multiple sync or async tasks
+    };
+
+    init().finally(async () => {
+      await BootSplash.hide({fade: true});
+      console.log('BootSplash has been hidden successfully');
+    });
+  }, []);
   return <TabNavigator />;
 }
